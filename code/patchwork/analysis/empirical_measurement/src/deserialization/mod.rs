a2f891dbe91ca573d3b1b7a43350ea6fa09518d2
NicoZeitz <63420648+NicoZeitz@users.noreply.github.com>
2024-01-21 12:23:35 +0100

    project for analysis

---
+++ code/patchwork/analysis/empirical_measurement/src/deserialization/mod.rs
@@ -0,0 +1,71 @@
+use std::num::NonZeroUsize;
+
+use patchwork_core::{ActionId, Patchwork};
+
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub(crate) struct Game {
+    pub turns: Vec<GameTurn>,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub(crate) struct GameTurn {
+    pub state: Patchwork,
+    pub action: Option<ActionId>,
+}
+
+pub(crate) struct GameLoader {
+    rx: std::sync::mpsc::Receiver<Game>,
+    loaded: std::sync::Arc<std::sync::atomic::AtomicUsize>,
+}
+
+impl GameLoader {
+    pub(crate) fn new(path: &std::path::PathBuf, parallelism: Option<NonZeroUsize>) -> Self {
+        let dir = std::fs::read_dir(path).unwrap();
+        let parallelism = parallelism.unwrap_or_else(|| std::thread::available_parallelism().unwrap());
+        let (tx, rx) = std::sync::mpsc::channel();
+        let loaded = std::sync::Arc::new(std::sync::atomic::AtomicUsize::new(0));
+
+        let thread_pool = rayon::ThreadPoolBuilder::new()
+            .num_threads(parallelism.get())
+            .build()
+            .unwrap();
+
+        for entry in dir {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if path.is_dir() {
+                continue;
+            }
+
+            let tx = tx.clone();
+            let loaded = loaded.clone();
+            thread_pool.spawn(move || {
+                while loaded.load(std::sync::atomic::Ordering::Relaxed) > (parallelism.get() + 2) * 10_000 {
+                    std::thread::sleep(std::time::Duration::from_millis(100));
+                    std::thread::yield_now();
+                }
+
+                let bytes = std::fs::read(path).unwrap();
+                let game_chunks: Vec<Game> = bincode::deserialize(&bytes).unwrap();
+                for game_chunk in game_chunks {
+                    loaded.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                    if tx.send(game_chunk).is_err() {
+                        break;
+                    };
+                }
+            });
+        }
+
+        Self { rx, loaded }
+    }
+}
+
+impl Iterator for GameLoader {
+    type Item = Game;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let item = self.rx.recv().ok();
+        self.loaded.fetch_sub(1, std::sync::atomic::Ordering::Relaxed);
+        item
+    }
+}
--- code/patchwork/analysis/empirical_measurement/src/main.rs
+++ code/patchwork/analysis/empirical_measurement/src/main.rs
@@ -1,174 +1,136 @@
-use std::io::Write;
+mod deserialization;
 
-use patchwork_core::{ActionId, Patchwork};
+use patchwork_core::TurnType;
 
-#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
-struct Game {
-    pub turns: Vec<GameTurn>,
-}
+use crate::deserialization::GameLoader;
 
-#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
-struct GameTurn {
-    pub state: Patchwork,
-    pub action: Option<ActionId>,
-}
+fn get_game_statistics(input: &std::path::PathBuf, output: &std::path::Path, gather: Gather) {
+    let mut game_length_writer = if gather.game {
+        Some(
+            csv::WriterBuilder::new()
+                .has_headers(false)
+                .from_path(output.join("game_lengths.csv"))
+                .unwrap(),
+        )
+    } else {
+        None
+    };
+    let mut available_actions_writer = if gather.available_actions {
+        Some(
+            csv::WriterBuilder::new()
+                .has_headers(false)
+                .from_path(output.join("available_actions.csv"))
+                .unwrap(),
+        )
+    } else {
+        None
+    };
 
-#[allow(clippy::never_loop)]
-fn get_game_statistics(path: &std::path::PathBuf) {
-    let mut lengths = vec![];
-    let mut min_length = std::usize::MAX;
-    let mut max_length = std::usize::MIN;
-    let mut sum_length = 0;
+    println!("Getting game statistics from {:?}", input);
     let mut games = 0;
-
-    let mut last_round = std::time::Instant::now();
-
-    for game in GameIterator::new(path, true) {
-        if last_round.elapsed().as_nanos() >= 1_000_000_000 {
-            println!("Time since last round: {:?}", last_round.elapsed());
+    for game in GameLoader::new(input, None) {
+        // Game length writer
+        if gather.game {
+            let game_length = game.turns.iter().filter(|turn| turn.action.is_some()).count();
+            game_length_writer.as_mut().unwrap().serialize((game_length,)).unwrap();
         }
 
-        let length = game.turns.iter().filter(|turn| turn.action.is_some()).count();
+        // Available actions writer
+        // This slows down the process a lot as we need to calculate the available actions for each state
+        if gather.available_actions {
+            let available_actions = game
+                .turns
+                .iter()
+                .map(|turn| turn.state.clone())
+                .filter(|state| !matches!(state.turn_type, TurnType::SpecialPatchPlacement) && !state.is_terminated())
+                .map(|state| state.get_valid_actions())
+                .filter(|actions| {
+                    if actions.is_empty() {
+                        return false;
+                    }
 
-        lengths.push(length);
-        min_length = std::cmp::min(min_length, length);
-        max_length = std::cmp::max(max_length, length);
-        sum_length += length;
+                    if actions[0].is_special_patch_placement() {
+                        return false;
+                    }
+
+                    true
+                })
+                .map(|actions| actions.len());
+            for available_action in available_actions {
+                available_actions_writer
+                    .as_mut()
+                    .unwrap()
+                    .serialize((available_action,))
+                    .unwrap();
+            }
+        }
+
+        // game.turns
+        //     .iter()
+        //     .map(|turn| turn.action)
+        //     .filter_map(|action| action)
+        //     .filter(|action| !action.is_special_patch_placement());
+
         games += 1;
-
-        let avg_length = sum_length as f64 / games as f64;
-
-        // print!("\x1b[4A\r");
-        // println!("================= Game {} =================", games);
-        // println!("Current Minimum game length: {}", min_length);
-        // println!("Current Maximum game length: {}", max_length);
-        // println!("Current Average game length: {}", avg_length);
-        std::io::stdout().flush().unwrap();
-
-        last_round = std::time::Instant::now();
-    }
-
-    let avg_length = sum_length as f64 / games as f64;
-
-    println!("================= Ending statistics {} =================", games);
-    println!("Getting game statistics from {:?}", path);
-    println!("Minimum game length: {}", min_length);
-    println!("Maximum game length: {}", max_length);
-    println!("Average game length: {}", avg_length);
-}
-
-fn get_action_statistics(path: &std::path::PathBuf) {
-    GameIterator::new(path, true);
-
-    println!("Getting action statistics from {:?}", path);
-}
-
-struct GameIterator {
-    multithreaded: bool,
-    dir: std::fs::ReadDir,
-    game_chucks: Vec<Game>,
-}
-
-impl GameIterator {
-    fn new(path: &std::path::PathBuf, multithreaded: bool) -> Self {
-        let dir: std::fs::ReadDir = std::fs::read_dir(path).unwrap();
-        Self {
-            dir,
-            game_chucks: vec![],
-            multithreaded,
+        if games % 10000 == 0 {
+            print!("\r================= Game {} =================", games);
         }
     }
+    println!("\r================= FINISHED GATHERING STATISTICS =================");
 }
 
-impl Iterator for GameIterator {
-    type Item = Game;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            if !self.game_chucks.is_empty() {
-                return self.game_chucks.pop();
-            }
-
-            if self.multithreaded {
-                std::thread::scope(|s| {
-                    let mut handles = vec![];
-                    self.game_chucks = vec![];
-
-                    for _ in 0..(std::thread::available_parallelism().unwrap().get() - 1) {
-                        let Some(entry) = self.dir.next() else {
-                            break;
-                        };
-
-                        let entry = entry.unwrap();
-                        let path = entry.path();
-                        if path.is_dir() {
-                            continue;
-                        }
-
-                        let file = std::fs::File::open(path).unwrap();
-                        handles.push(s.spawn(move || {
-                            let game_chucks: Vec<Game> = bincode::deserialize_from(file).unwrap();
-                            game_chucks
-                        }));
-                    }
-
-                    for handle in handles {
-                        self.game_chucks.extend(handle.join().unwrap());
-                    }
-                });
-            } else {
-                let Some(entry) = self.dir.next() else {
-                    return None;
-                };
-
-                let entry = entry.unwrap();
-                let path = entry.path();
-                if path.is_dir() {
-                    continue;
-                }
-
-                let file = std::fs::File::open(path).unwrap();
-                let game_chucks: Vec<Game> = bincode::deserialize_from(file).unwrap();
-                self.game_chucks = game_chucks;
-            }
-        }
-    }
+struct Gather {
+    game: bool,
+    available_actions: bool,
 }
 
 fn main() {
     let cmd = clap::Command::new("empirical-measurement")
         .bin_name("empirical-measurement")
         .about("Gets different empirical measurements from a set of recorded games")
-        .subcommand_required(true)
-        .subcommand(
-            clap::Command::new("game")
-                .about("Gets the minimum, maximum and average number of plys in a set of recorded games")
-                .arg(
-                    clap::Arg::new("path")
-                        .short('p')
-                        .long("path")
-                        .help("The path to the directory where the recorded games are")
-                        .required(true)
-                        .value_parser(clap::value_parser!(std::path::PathBuf)),
-                ),
+        .arg(
+            clap::Arg::new("in")
+                .short('i')
+                .long("input")
+                .alias("in")
+                .help("The path to the directory where the recorded games are")
+                .required(true)
+                .value_parser(clap::value_parser!(std::path::PathBuf)),
         )
-        .subcommand(
-            clap::Command::new("action")
-            .about("Gets the minimum, maximum and average number of available actions in a set of recorded games (special patch placement actions excluded)")
-                .arg(
-                    clap::Arg::new("path")
-                        .short('p')
-                        .long("path")
-                        .help("The path to the directory where the recorded games are")
-                        .required(true)
-                        .value_parser(clap::value_parser!(std::path::PathBuf)),
-                ),
+        .arg(
+            clap::Arg::new("out")
+                .short('o')
+                .long("output")
+                .alias("out")
+                .help("The path to the directory where to store the extracted statistical data")
+                .required(true)
+                .value_parser(clap::value_parser!(std::path::PathBuf)),
+        )
+        .arg(
+            // List all things to gather e.g. --game --available-actions
+            clap::Arg::new("game")
+                .long("game")
+                .alias("game")
+                .default_value("false")
+                .value_parser(clap::value_parser!(bool))
+                .help("Gathers statistics about the length of games"),
+        )
+        .arg(
+            clap::Arg::new("available-actions")
+                .long("available-actions")
+                .alias("available-actions")
+                .default_value("false")
+                .value_parser(clap::value_parser!(bool))
+                .help("Gathers statistics about the number of available actions per turn"),
         );
 
     let matches = cmd.get_matches();
-    match matches.subcommand() {
-        Some(("game", matches)) => get_game_statistics(matches.get_one::<std::path::PathBuf>("path").unwrap()),
-        Some(("action", matches)) => get_action_statistics(matches.get_one::<std::path::PathBuf>("path").unwrap()),
-        _ => unreachable!("clap should ensure we don't get here"),
-    };
+    get_game_statistics(
+        matches.get_one::<std::path::PathBuf>("in").unwrap(),
+        matches.get_one::<std::path::PathBuf>("out").unwrap(),
+        Gather {
+            game: *matches.get_one::<bool>("game").unwrap_or(&false),
+            available_actions: *matches.get_one::<bool>("available-actions").unwrap_or(&false),
+        },
+    );
 }
