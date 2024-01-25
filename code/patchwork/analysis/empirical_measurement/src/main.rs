fca1ae153b687df2d474c5ce54f023d68a49149a
NicoZeitz <63420648+NicoZeitz@users.noreply.github.com>
2024-01-25 14:53:47 +0100

    update

--- code/patchwork/analysis/empirical_measurement/src/main.rs
+++ code/patchwork/analysis/empirical_measurement/src/main.rs
@@ -1,11 +1,11 @@
 mod deserialization;
 
-use patchwork_core::TurnType;
+use patchwork_core::{PatchManager, TerminationType, TurnType};
 
 use crate::deserialization::GameLoader;
 
 fn get_game_statistics(input: &std::path::PathBuf, output: &std::path::Path, gather: Gather) {
-    if !gather.available_actions && !gather.game && !gather.available_special_actions {
+    if !gather.has_something() {
         println!("Nothing to gather");
         return;
     }
@@ -46,10 +46,14 @@ fn get_game_statistics(input: &std::path::PathBuf, output: &std::path::Path, gat
     } else {
         None
     };
+    let mut action_scores_map = std::collections::HashMap::new();
 
     println!("Getting game statistics from {:?}", input);
     let mut games = 0;
+    let mut no_games = true;
     for game in GameLoader::new(input, None) {
+        no_games = false;
+
         // Game length writer
         if gather.game {
             let game_length = game.turns.iter().filter(|turn| turn.action.is_some()).count();
@@ -122,17 +126,109 @@ fn get_game_statistics(input: &std::path::PathBuf, output: &std::path::Path, gat
             }
         }
 
-        // game.turns
-        //     .iter()
-        //     .map(|turn| turn.action)
-        //     .filter_map(|action| action)
-        //     .filter(|action| !action.is_special_patch_placement());
+        // Action scores writer
+        if gather.action_scores {
+            let end_state = &game.turns.last().unwrap().state;
+            assert!(
+                end_state.is_terminated(),
+                "[get_game_statistics(action_scores)] Game is not terminated"
+            );
+
+            let result = end_state.get_termination_result();
+
+            game.turns.iter().filter(|turn| turn.action.is_some()).for_each(|turn| {
+                let action = turn.action.unwrap();
+                let is_player_1 = turn.state.is_player_1();
+
+                let score = match result.termination {
+                    TerminationType::Player1Won => {
+                        if is_player_1 {
+                            1
+                        } else {
+                            -1
+                        }
+                    }
+                    TerminationType::Player2Won => {
+                        if is_player_1 {
+                            -1
+                        } else {
+                            1
+                        }
+                    }
+                };
+
+                // TODO: look at with ply offset
+                // TODO: look at actual_score vs only score
+
+                let actual_score = score * ((result.player_1_score - result.player_2_score).abs() + 1);
+                let key = if action.is_walking() {
+                    0
+                } else if action.is_special_patch_placement() {
+                    action.get_quilt_board_index() as u32 + 1
+                } else if action.is_patch_placement() {
+                    action.get_patch_id() as u32 * PatchManager::MAX_AMOUNT_OF_TRANSFORMATIONS
+                        + action.get_patch_transformation_index() as u32
+                        + 82
+                } else {
+                    unreachable!(
+                        "[get_game_statistics(action_scores)] Other actions types should not be in the dataset"
+                    )
+                };
+                let description = if action.is_walking() {
+                    "walking".to_string()
+                } else if action.is_special_patch_placement() {
+                    format!("special_patch_placement({})", action.get_quilt_board_index())
+                } else if action.is_patch_placement() {
+                    format!(
+                        "patch_placement({}, {})",
+                        action.get_patch_id(),
+                        action.get_patch_transformation_index()
+                    )
+                } else {
+                    unreachable!(
+                        "[get_game_statistics(action_scores)] Other actions types should not be in the dataset"
+                    )
+                };
+
+                let entry = action_scores_map.entry(key).or_insert((description, 0, 0));
+                entry.1 += actual_score;
+                entry.2 += score;
+            });
+        }
 
         games += 1;
         if games % 10000 == 0 {
             print!("\r================= Game {} =================", games);
         }
     }
+
+    if no_games {
+        println!("No games found");
+    }
+
+    if gather.action_scores {
+        println!("Running post processing for action scores");
+        let mut action_scores_writer = csv::WriterBuilder::new()
+            .has_headers(false)
+            .from_path(output.join("action_scores.csv"))
+            .unwrap();
+
+        let mut action_scores_win_loss_writer = csv::WriterBuilder::new()
+            .has_headers(false)
+            .from_path(output.join("action_scores_win_loss.csv"))
+            .unwrap();
+
+        let mut data_vector = action_scores_map.values().collect::<Vec<_>>();
+        data_vector.sort_by_key(|(desc, _, _)| desc);
+
+        for (description, score, win_loss) in data_vector {
+            action_scores_writer.serialize((description, score)).unwrap();
+            action_scores_win_loss_writer
+                .serialize((description, win_loss))
+                .unwrap();
+        }
+    }
+
     println!("\r================= FINISHED GATHERING STATISTICS =================");
 }
 
@@ -140,6 +236,13 @@ struct Gather {
     game: bool,
     available_actions: bool,
     available_special_actions: bool,
+    action_scores: bool,
+}
+
+impl Gather {
+    pub fn has_something(&self) -> bool {
+        self.game || self.available_actions || self.available_special_actions || self.action_scores
+    }
 }
 
 fn main() {
@@ -186,6 +289,13 @@ fn main() {
                 .required(false)
                 .num_args(0)
                 .help("Gathers statistics about the number of available special actions per turn"),
+        )
+        .arg(
+            clap::Arg::new("action-scores")
+                .long("action-scores")
+                .required(false)
+                .num_args(0)
+                .help("Gathers statistics about the scores of actions"),
         );
 
     let matches = cmd.get_matches();
@@ -196,6 +306,7 @@ fn main() {
             game: matches.get_flag("game"),
             available_actions: matches.get_flag("available-actions"),
             available_special_actions: matches.get_flag("available-special-actions"),
+            action_scores: matches.get_flag("action-scores"),
         },
     );
 }
